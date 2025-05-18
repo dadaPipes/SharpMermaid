81a3a3adcc33b27fdd02727d7a864fe8779264b9
dadaPipes <markoliverstockflethstaf@gmail.com>
2025-05-18 10:46:35 +0200

    refactor(SlnModel) readability

--- SharpMermaid/SlnModel.cs
+++ SharpMermaid/SlnModel.cs
@@ -3,24 +3,23 @@ class SlnModel
 {
     public readonly string Name;
     public readonly string Directory;
-    public List<CsprojModel> Csprojs { get; } = [];
+    public List<CsprojModel> Csprojs { get; }
     public bool HasProjects => Csprojs.Count > 0;
 
     public SlnModel(string slnFullPath)
     {
         Directory = Path.GetDirectoryName(slnFullPath) ?? string.Empty;
-        Name = Path.GetFileNameWithoutExtension(slnFullPath);
-        LoadProjectFilesFromSolution();
+        Name      = Path.GetFileNameWithoutExtension(slnFullPath);
+        Csprojs   = LoadProjectFilesFromSolution();
     }
 
-    void LoadProjectFilesFromSolution()
+    List<CsprojModel> LoadProjectFilesFromSolution()
     {
-        foreach (var csprojFullPath in System.IO.Directory.GetFiles(Directory, "*.csproj", SearchOption.AllDirectories))
-        {
-            var relativePathFromSolution = Path.GetRelativePath(Directory, csprojFullPath);
-            var name = Path.GetFileNameWithoutExtension(csprojFullPath);
-
-            Csprojs.Add(new CsprojModel(name, csprojFullPath, relativePathFromSolution));
-        }
+        return System.IO.Directory.GetFiles(Directory, "*.csproj", SearchOption.AllDirectories)
+            .Select(csprojFullPath => new CsprojModel(
+                name               : Path.GetFileNameWithoutExtension(csprojFullPath),
+                fullPath           : csprojFullPath,
+                relativePathFromSln: Path.GetRelativePath(Directory, csprojFullPath)))
+            .ToList();
     }
 }
